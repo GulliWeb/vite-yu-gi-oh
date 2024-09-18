84d2e71aa3a54a37ff6b1dc66749b9fd30193a6c
Andrea Gulli <andreagulli11@gmail.com>
2024-09-18 15:36:40 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+    <H1>CIAO, IO SONO UN HEADER</H1>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+    <H1>CIAO, IO SONO UN MAIN</H1>
+
+</template>
+
+<style scoped>
+</style>
