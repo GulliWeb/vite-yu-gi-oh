746ce07a06af17bfebec38f88afc4a369bf53655
Andrea Gulli <andreagulli11@gmail.com>
2024-09-18 18:46:54 +0200

    API call

--- src/components/MainMonsterList.vue
+++ src/components/MainMonsterList.vue
@@ -1,24 +1,42 @@
 <script>
+import axios from 'axios';
 import MainMonsterListCard from './MainMonsterListCard.vue';
 export default {
   data() {
     return {
+      monsterList: [],
+      axiosUrl : "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0"
       
     }
   }, 
+  methods:{
+    // Metodo per fare una chiamata API con axios
+    getMonsters(){
+      axios.get(this.axiosUrl)
+      .then((response) => {
+        console.dir(response.data.data);
+        this.monsterList = response.data.data
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+        }
+      },
   components:{
     MainMonsterListCard
+  },
+  created(){
+    this.getMonsters()
   }
 }
 </script>
 
 <template>
   <div class="card-wrapper">
-    <MainMonsterListCard/>
+    <div class="card-row">
+      <MainMonsterListCard/>
+    </div>
   </div>
-
-
-  
 </template>
 
 <style lang="scss" scoped>
@@ -27,7 +45,7 @@ export default {
   display: flex;
   flex-wrap: wrap;
   height: calc(100vh - 190px);
-  max-width: 1200px;
+  max-width: 1400px;
   margin: 0 auto;
   backdrop-filter: blur(3px);
   overflow: auto;
--- src/components/MainMonsterListCard.vue
+++ src/components/MainMonsterListCard.vue
@@ -14,73 +14,13 @@ export default {
     <h3>Monster name</h3>
     <p>species</p>
   </div>
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  <div class="card">
-    <img src="" alt="">
-    <h3>Monster name</h3>
-    <p>species</p>
-  </div>
-  
-  
-
 </template>
 
 <style lang="scss" scoped>
 .card{
   width: 250px;
+  margin-right: 20px;
+  margin-bottom: 10px;
   height: 350px;
   border: 1px solid black;
 }
